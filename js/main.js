9b3e505998250a3d8f058899e1d1a2ec246962bf
jeroenheijmans <jeroenheijmans@localhost>
2013-01-15 00:39:19 +0100

    Added some prototype-worthy javascript for a working demo.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-
+(function( $ ) {
+	$('#initiative-next').on('click', function() {
+		var current = $('#initiative table tbody .active');
+		var next = current.next('tr');
+		
+		if (next.size() === 0) {
+			next = $('#initiative table tbody tr:first');
+			$().battleTop('nextRound');
+		}
+		
+		next.addClass('active').addClass('expanded').find('.collapse-expand-row');
+		current.removeClass('active').removeClass('expanded').find('.collapse-expand-row');
+	});
+	
+	$(document).on('click', '.collapse-expand-row', function() {
+		$(this).closest('tr').toggleClass('expanded');
+	});
+})( jQuery );
--- js/plugins.js
+++ js/plugins.js
@@ -21,4 +21,153 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
+(function( $ ) {
+	
+	var model = {
+		combat : {	
+			characters : [
+				{ 	name : 'Skaak',
+					currentHitPoints : 106,
+					maxHitPoints : 106,
+					currentInitiative : 22,
+					initiativeModifier : 5,
+					conditions : [
+						{ name : 'haste', roundsLeft : 16 }
+					]
+				},
+				{	name : 'Elcade',
+					currentHitPoints : 24,
+					maxHitPoints : 89,
+					currentInitiative : 21,
+					initiativeModifier : 6,
+					conditions : [
+						{ name : 'frightened', roundsLeft : 2 },
+						{ name : 'dazed', roundsLeft : 1 }
+					]
+				},
+				{ 	name : 'Oscar',
+					currentHitPoints : 66,
+					maxHitPoints : 105,
+					currentInitiative : 21,
+					initiativeModifier : 4,
+					conditions : []
+				},
+				{ 	name : 'Kagor',
+					currentHitPoints : 82,
+					maxHitPoints : 82,
+					currentInitiative : -1,
+					initiativeModifier : -2,
+					conditions : [
+						{ name : 'frightened', roundsLeft : 1 }
+					]
+				},
+				{ 	name : 'Ancient red dragon',
+					currentHitPoints : -25,
+					maxHitPoints : '???',
+					currentInitiative : 12,
+					initiativeModifier : 1,
+					conditions : [
+						{ name : 'slowed', roundsLeft : 8 }
+					]
+				}
+			],
+			currentRound : 1
+		},
+		isInDmMode : false,
+		currentMode : 'initiative'
+	};
+	
+	function setModelDmMode(isInDmMode) {
+		$('#dm-mode').prop('checked', isInDmMode);		
+	}
+	
+	function setActiveMode(mode) {
+		$('#edit-mode li').removeClass('active');
+		$('#mode-' + mode).addClass('active');
+	}
+	
+	function setcurrentRound(roundNumber) {
+		$('#initiative-current-round-number').text(roundNumber);
+		$('#current-round')
+			.css('opacity', '0.2')
+			.animate({ opacity: '1.0' }, 500)
+			.animate({ opacity: '0.2' }, 500)
+			.animate({ opacity: '1.0' }, 500);
+	}
+	
+	function rebuildInitiativeList(characters) {
+		// TODO: Refactor to some fancy pattern like MVVM? Anyways, here goes rapid prototyping:
+		
+		$('#initiative table tbody').empty();
+		
+		characters.sort(function(a,b) { return b.currentInitiative - a.currentInitiative; });
+	
+		for (i = 0; i < characters.length; i++) {
+			var conditions = characters[i].conditions.length > 0 ? $('<ul class="conditions">') : "";
+			for (j = 0; j < characters[i].conditions.length; j++) {
+				conditions.append($('<li>')
+					.text(characters[i].conditions[j].name
+							+ ' ('
+							+ characters[i].conditions[j].roundsLeft
+							+ ' round(s) left)'
+						)
+					);
+			}
+		
+			$('#initiative tbody')
+				.append(
+					$('<tr>')
+					.append(
+						$('<td class="actions"><button class="collapse-expand-row"></button></td>')
+						.after(
+							$('<td class="character">')
+							.text(characters[i].name)
+							.append(conditions)
+						)
+						.after($('<td class="number hp">').text(characters[i].currentHitPoints))
+						.after($('<td class="number initiative-total">').text(characters[i].currentInitiative))
+						.after($('<td class="number initiative-modifier">').text(characters[i].initiativeModifier))
+					)
+				);
+		}
+		
+		$('#initiative tbody tr:first').addClass('active').addClass('expanded');
+		$('#initiative tbody tr:first .collapse-expand-row');
+	}
+	
+	function decrementConditionDurations(character) {
+		for (i = character.conditions.length - 1; i >= 0; i--) {
+			character.conditions[i].roundsLeft--;
+			if (character.conditions[i].roundsLeft < 0) {
+				character.conditions.splice(i,1);
+			}
+		}				
+	}
+	
+	var methods = {
+		init : function () {	
+			setModelDmMode(model.isInDmMode);
+			setActiveMode(model.currentMode);
+			setcurrentRound(model.combat.currentRound);
+			rebuildInitiativeList(model.combat.characters);
+		},
+		
+		nextRound : function () {
+			model.combat.currentRound++;
+			model.combat.characters.forEach(decrementConditionDurations);
+			setcurrentRound(model.combat.currentRound);
+			rebuildInitiativeList(model.combat.characters);
+		}
+	};
+	
+	$.fn.battleTop = function(method) {
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.battleTop' );
+		}  
+	};
+	
+})( jQuery );
