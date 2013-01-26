73fc2b22f2c0c295028589024dea6d4b10c7a66e
jeroenheijmans <jeroenheijmans@localhost>
2013-01-26 17:11:51 +0100

    Adding characters more robust / less "requiring". Changed example characters.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+﻿(function( $ ) {	
+	$(document).on('click', '.readonly-pane', function() {
+		$(this).addClass('hidden');
+		$(this).siblings('.edit-pane').removeClass('hidden');
+	});	
+	
+	$(document).on('click', '.edit-pane .finish-editing', function() {
+		$(this).parent('.edit-pane').addClass('hidden');
+		$(this).parent('.edit-pane').siblings('.readonly-pane').removeClass('hidden');
+	});
+})( jQuery );
+
+$(document).ready(function() {
+	$('#main').battleTop();
+});
