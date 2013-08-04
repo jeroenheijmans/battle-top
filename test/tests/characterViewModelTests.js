d2c09769ca352195122291dbff6ff5fa3db81d92
jeroenheijmans <jeroenheijmans@localhost>
2013-08-04 12:36:12 +0200

    Putting view models under test. (work in progress)

---
+++ test/tests/characterViewModelTests.js
@@ -0,0 +1,81 @@
+﻿(function() {
+
+    var combat = null;
+
+    module("Character View Model", {
+        setup: function() {
+            // Mock the combat object
+            combat = {
+                activeCharacter: ko.observable(null)
+            };
+            combat.activeCharacter(new battleTop.viewModels.characterViewModel({name: 'Mialee'}, combat));
+        }
+    });
+
+
+    test( "Can construct default character view model", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({}, combat);
+        
+        deepEqual(viewModel.id(), 0);
+        deepEqual(viewModel.characterType(), "PC");
+        deepEqual(viewModel.initiativeState(), "normal");
+        deepEqual(viewModel.conditions().length, 0);
+        ok(!viewModel.isExpanded());
+    });
+    
+    
+    test( "Can toggle expanded state to true", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({}, combat);
+        viewModel.toggleExpandedState();
+        ok(viewModel.isExpanded());
+    });
+    
+    
+    test( "Can become active character", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({}, combat);
+        combat.activeCharacter(viewModel);
+        ok(viewModel.isActive());
+    });
+    
+    
+    test( "Beginning turn resets initiative state", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({initiativeState: 'delayed'}, combat);
+        viewModel.beginTurn();
+        deepEqual(viewModel.initiativeState(), 'normal');
+    });
+    
+    
+    test( "Beginning turn lowers condition rounds left", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({}, combat);
+        viewModel.conditions.push(new battleTop.viewModels.conditionViewModel({roundsLeft: 10}));
+        viewModel.conditions.push(new battleTop.viewModels.conditionViewModel({roundsLeft: 1}));
+        
+        viewModel.beginTurn();
+        deepEqual(viewModel.conditions().length, 2);
+        deepEqual(viewModel.conditions()[0].roundsLeft(), 9);
+        deepEqual(viewModel.conditions()[1].roundsLeft(), 0);
+    });
+    
+    
+    test( "Ending turn removes conditions with -1 rounds left", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({}, combat);
+        viewModel.conditions.push(new battleTop.viewModels.conditionViewModel({roundsLeft: -1}));
+        
+        viewModel.endTurn();
+        deepEqual(viewModel.conditions().length, 0);
+    });
+    
+    
+    test( "PC is considered player character", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({characterType: 'PC'}, combat);
+        ok(viewModel.isPlayerCharacter());
+    });
+    
+    
+    test( "Monster is not considered player character", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({characterType: 'monster'}, combat);
+        ok(!viewModel.isPlayerCharacter());
+    });
+    
+
+})();
---
+++ test/tests/hpChangeViewModelTests.js
@@ -0,0 +1,37 @@
+﻿module("HP Change View Model");
+
+
+test( "Default type of HP change is 'damage'", function() {
+    var viewModel = new battleTop.viewModels.hpChangeViewModel();
+    deepEqual(viewModel.hpChangeType(), "damage");
+});
+
+
+test( "Default amount of HP change is 1", function() {
+    var viewModel = new battleTop.viewModels.hpChangeViewModel();
+    deepEqual(viewModel.hpChangeAbsolute(), 1);
+});
+
+
+test( "HP change of type 'damage' gives negative HP change", function() {
+    var data = {
+        hpChangeType: 'damage',
+        hpChangeAbsolute: 42
+    };
+
+    var viewModel = new battleTop.viewModels.hpChangeViewModel(data);
+    
+    deepEqual(viewModel.hpChangeTotal(), -42);
+});
+
+
+test( "HP change of type 'healing' gives positive HP change", function() {
+    var data = {
+        hpChangeType: 'healing',
+        hpChangeAbsolute: 42
+    };
+
+    var viewModel = new battleTop.viewModels.hpChangeViewModel(data);
+    
+    deepEqual(viewModel.hpChangeTotal(), 42);
+});
