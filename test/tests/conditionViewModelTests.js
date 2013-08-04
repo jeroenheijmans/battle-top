6396329ad41dba2264b8a0ceabe3058c1ce7de91
jeroenheijmans <jeroenheijmans@localhost>
2013-08-04 12:36:12 +0200

    Putting view models under test. (work in progress)

---
+++ test/tests/conditionViewModelTests.js
@@ -0,0 +1,27 @@
+﻿module("Condition View Model");
+
+
+test( "Default number of rounds left is 1", function() {
+    var viewModel = new battleTop.viewModels.conditionViewModel();
+    deepEqual(viewModel.roundsLeft(), 1);
+});
+
+
+test( "Constructor can take data argument with defaults", function() {
+    var data = {
+        name: 'Dazed',
+        roundsLeft: 3
+    };
+    
+    var viewModel = new battleTop.viewModels.conditionViewModel(data);
+    
+    deepEqual(viewModel.name(), 'Dazed');
+    deepEqual(viewModel.roundsLeft(), 3);
+});
+
+
+test( "Rounds left indication will include parentheses and turns-character", function() {
+    var viewModel = new battleTop.viewModels.conditionViewModel();
+    viewModel.roundsLeft(3);
+    deepEqual(viewModel.roundsLeftIndication(), '(3↺)');
+});
