76a0cb7c1fecb993cbf282553e134bef26e292f2
jeroenheijmans <jeroenheijmans@localhost>
2013-08-04 15:08:52 +0200

    Testing dice utils

---
+++ test/tests/diceTests.js
@@ -0,0 +1,28 @@
+﻿(function() {
+    var browserRandom = Math.random;
+
+    module("Character View Model", {
+        setup: function() {
+            // Mock the random functionality
+            Math.random = function() { return 0.5; };
+        }, teardown: function() {
+            Math.random = browserRandom;
+        }
+    });
+
+    test( "Default dice roll is single d20 roll", function() {
+        var result = battleTop.util.dice.rollDice();
+        deepEqual(result, 11);
+    });
+
+    test( "Can roll two d20s", function() {
+        var result = battleTop.util.dice.rollDice(20, 2);
+        deepEqual(result, 22);
+    });
+
+    test( "Can roll two d6s", function() {
+        var result = battleTop.util.dice.rollDice(6, 2);
+        deepEqual(result, 8);
+    });
+    
+})();
---
+++ test/tests/extensionsTests.js
@@ -0,0 +1,7 @@
+﻿module("Date extension tests");
+
+test( "Can get formatted time", function() {
+    var myDate = new Date(2013, 6, 23, 13, 6, 4);
+    
+    deepEqual(myDate.getFormattedTime(), '13:06:04');
+});
