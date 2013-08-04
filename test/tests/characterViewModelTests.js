270226f43dd11865581831aa7849a42d917f52ee
jeroenheijmans <jeroenheijmans@localhost>
2013-08-04 13:08:44 +0200

    Finished adding tests for most of the character view model. A few tests still fail, but the tests are okay so the VM needs to be changed.

--- test/tests/characterViewModelTests.js
+++ test/tests/characterViewModelTests.js
@@ -77,5 +77,83 @@
         ok(!viewModel.isPlayerCharacter());
     });
     
+    
+    test( "Will format positive initiative modifier with plus sign", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({initiativeModifier: 4}, combat);
+        deepEqual(viewModel.initiativeModifierAsAside(), '(+4)');     
+    });
+    
+    
+    test( "Will format negative initiative modifier with minus sign", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({initiativeModifier: -2}, combat);
+        deepEqual(viewModel.initiativeModifierAsAside(), '(-2)');     
+    });
+    
+    
+    test( "Will format zero initiative modifier with plus sign", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({initiativeModifier: 0}, combat);
+        deepEqual(viewModel.initiativeModifierAsAside(), '(+0)');     
+    });
+    
+    
+    test( "Will display empty string for undefined hit points", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({}, combat);
+        deepEqual(viewModel.hitPoints(), '');
+    });
+    
+    
+    test( "Will set up a new condition to add", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({}, combat);
+        notDeepEqual(viewModel.conditionToAdd(), undefined);
+        notDeepEqual(viewModel.conditionToAdd(), null);
+    });
+    
+    
+    test( "Adding new condition will add to the list and reset the 'to add' condition", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({}, combat);
+        
+        viewModel.conditionToAdd().name("Dazed");
+        viewModel.conditionToAdd().roundsLeft(1);
+        viewModel.addCondition();
+        
+        deepEqual(viewModel.conditions().length, 1);
+        notDeepEqual(viewModel.conditionToAdd(), undefined);
+        notDeepEqual(viewModel.conditionToAdd(), null);
+        notDeepEqual(viewModel.conditionToAdd().name(), viewModel.conditions()[0].name());
+    });
+    
+    
+    test( "Can remove a condition", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({}, combat);
+        var condition = new battleTop.viewModels.conditionViewModel();
+        
+        viewModel.conditions.push(condition);
+        deepEqual(viewModel.conditions().length, 1);
+        
+        viewModel.removeCondition(condition);
+        deepEqual(viewModel.conditions().length, 0);
+    });
+    
+    
+    test( "Change Hit Points will update hit points with current change view model", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({currentHitPoints: 8}, combat);
+        
+        viewModel.hpChange().hpChangeAbsolute(2);
+        viewModel.changeHitPoints();
+        
+        deepEqual(viewModel.currentHitPoints(), 6);
+    });
+    
+    
+    test( "Change Hit Points will reset current change view model", function() {
+        var viewModel = new battleTop.viewModels.characterViewModel({currentHitPoints: 8}, combat);
+        var defaultHpChange = viewModel.hpChange().hpChangeAbsolute();
+        
+        viewModel.hpChange().hpChangeAbsolute(2);
+        viewModel.changeHitPoints();
+        
+        deepEqual(viewModel.hpChange().hpChangeAbsolute(), defaultHpChange);
+    });
+    
 
 })();
